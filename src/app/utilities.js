0ce053529ac576709a675606124adce3bef82ea8
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-25 16:53:47 +0530

    added util

--- src/app/utilities.js
+++ src/app/utilities.js
@@ -2641,3 +2641,18 @@ export const TRIANGULATION = [
     255,
 ];
 
+export const drawMesh = (predictions, ctx) => {
+    if (predictions.length > 0) {
+        predictions.forEach(prediction => {
+            const keypoints = prediction.scaledMesh;
+            for (let i = 0; i < keypoints.length; i++) {
+                const x = keypoints[i][0]
+                const y = keypoints[i][1]
+                ctx.beginPath()
+                ctx.arc(x, y, 1, 0, 3 * Math.PI)
+                ctx.fillStyle = "aqua"
+                ctx.fill()
+            }
+        })
+    }
+}
